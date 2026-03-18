6e23d570fd5900ad1a666fa71289697d56827228
Jakub Ćwiek <s32631@pjwstk.edu.pl>
2026-03-18 22:15:08 +0100

    Dodano metode CalculateMin

--- ConsoleApp1/ConsoleApp1/StatisticsHelper.cs
+++ ConsoleApp1/ConsoleApp1/StatisticsHelper.cs
@@ -3,4 +3,7 @@ namespace ConsoleApp1;
 public class StatisticsHelper
 {
     public void CalculateAverage(int[] values) {}
+    
+    public void CalculateMax(int[] values) {}
+    public void CalculateMin(int[] values) {}
 }
